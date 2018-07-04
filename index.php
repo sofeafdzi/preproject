c2a2dd8458273e397258a30e77a5dba6644e1866
sofeafdzi <miera.sofea@yahoo.com,my>
2018-07-04 11:03:38 +0800

    first commit

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ include("inc/header.php");
 
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-7 col-lg-8 hidden-xs hidden-sm">
-				<h1 class="txt-color-red login-header-big">TNBR Pre-Project Management System</h1>
+				<h1 class="txt-color-red login-header-big">TNBR Pre-Project Management System </h1>
 				<div class="hero">
 
 					<div class="pull-left login-desc-box-l">
